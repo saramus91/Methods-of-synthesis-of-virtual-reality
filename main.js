eecbcfe2275f8bf8382d60c9c82d43999b61a096
Anton Bagniy <bagniy.antonio@gmail.com>
2025-05-21 23:35:43 +0300

    move initFullScreenQuad to a separate file

--- main.js
+++ main.js
@@ -97,39 +97,6 @@ function initShaders() {
     shProgram.iFullScreenQuad   = gl.getUniformLocation(prog, "uFullScreenQuad");
 }
 
-
-
-function initFullScreenQuad() {
-    fullscreenQuad.iVertexBuffer = gl.createBuffer();
-    fullscreenQuad.iTexCoordBuffer = gl.createBuffer();
-
-    let quadVerts = new Float32Array([
-        -1, -1,  0,
-        1, -1,  0,
-        1,  1,  0,
-
-        -1, -1,  0,
-        1,  1,  0,
-        -1,  1,  0
-    ]);
-    let quadTex = new Float32Array([
-        0, 0,
-        1, 0,
-        1, 1,
-
-        0, 0,
-        1, 1,
-        0, 1
-    ]);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, fullscreenQuad.iVertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, quadVerts, gl.STATIC_DRAW);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, fullscreenQuad.iTexCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, quadTex, gl.STATIC_DRAW);
-}
-
-
 function drawScene() {
     if (!gl) return;
 
